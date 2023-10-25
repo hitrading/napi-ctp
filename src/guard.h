75091ea1b5a64d9fd15e893d36ed6de4a6903ba6
南鲸 <southwhale@qq.com>
2023-10-26 00:06:23 +0800

    Guard类改名为DeferGuard
    增加AutoLock改进线程安全
    Guard类改名为DeferGuard

--- src/guard.h
+++ src/guard.h
@@ -13,19 +13,32 @@
 #define __GUARD_H__
 
 #include <functional>
+#include <uv.h>
 
-class Guard {
+#define __QUOTE__(name, id) name##id
+#define _QUOTE(name, id) __QUOTE__(name, id)
+
+class DeferGuard {
 public:
-  Guard(const std::function<void(void)> &func);
-  ~Guard();
+  inline DeferGuard(const std::function<void(void)> &func) : _func(func) {}
+  inline ~DeferGuard() { _func(); }
 
 private:
   std::function<void(void)> _func;
 };
 
-#define __QUOTE__(name, id) name##id
-#define _QUOTE(name, id) __QUOTE__(name, id)
+#define Defer DeferGuard _QUOTE(__guard_, __LINE__)
 
-#define Defer Guard _QUOTE(__guard_, __LINE__)
+class LockGuard {
+public:
+  inline LockGuard(uv_mutex_t &mutex) : _mutex(&mutex) { uv_mutex_lock(_mutex); }
+  inline LockGuard(uv_mutex_t *mutex) : _mutex(mutex) { uv_mutex_lock(_mutex); }
+  inline ~LockGuard() { uv_mutex_unlock(_mutex); }
+
+private:
+  uv_mutex_t *_mutex;
+};
+
+#define AutoLock LockGuard _QUOTE(__lock_, __LINE__)
 
 #endif /* __GUARD_H__ */
